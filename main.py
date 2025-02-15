646f6c1a589de0c0d2e3cf06d7480d5bb3a2568e
arturoaguerre <arturoaguerre@hotmail.com>
2025-02-15 03:10:36 -0300

    added fps delta time

--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    print("Starting asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0, 0, 0))
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+        
+        dt = clock.tick(60) / 1000
 
 
 
