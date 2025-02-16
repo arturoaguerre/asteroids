4632e61df9547d4aa5b62643bf80a8019b092325
arturoaguerre <arturoaguerre@hotmail.com>
2025-02-16 13:08:23 -0300

    added simple .kill() to asteroids and bullets

--- main.py
+++ main.py
@@ -38,6 +38,11 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
+
             if asteroid.collision(player):
                 print("Game over!")
                 return
